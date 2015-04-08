6f3cd2ec2f056f799f6f29480f60c267d19a21d3
NShively <njshively1354@gmail.com>
2015-04-08 01:03:47 -0700

    Updated FuelFinder parser. -SHOULD- work now. Finished cost calculator.

--- app/src/main/java/fuelfinder/mann/Parser/FuelSourceParser.java
+++ app/src/main/java/fuelfinder/mann/Parser/FuelSourceParser.java
@@ -4,6 +4,12 @@ import android.location.Location;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import fuelfinder.mann.Models.FuelPriceModel;
@@ -12,6 +18,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 
@@ -19,12 +29,67 @@ import java.util.ArrayList;
  * Created by Action Johnny on 4/1/2015.
  */
 public class FuelSourceParser {
-    static public ArrayList<FuelPriceModel> JSONtoModel(String Input){
-        ArrayList<FuelPriceModel> Models = new ArrayList();
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+    static public ArrayList<FuelPriceModel> JSONtoModel(Location myLoc){
+        HttpClient client = new DefaultHttpClient();
+
+        HttpGet request = new HttpGet("http://api.mygasfeed.com/stations/radius/"+myLoc.getLatitude()+"/"+myLoc.getLongitude()+"/3/reg/Price/wr6ntggmbw.json");
+        HttpResponse response;
+        String result = null;
+        try {
+            response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+
+                // A Simple JSON Response Read
+                InputStream instream = entity.getContent();
+                result = convertStreamToString(instream);
+                // now you have the string representation of the HTML request
+                instream.close();
+
+            }
+            // Headers
+            org.apache.http.Header[] headers = response.getAllHeaders();
+            for (int i = 0; i < headers.length; i++) {
+                System.out.println(headers[i]);
+            }
+        } catch (ClientProtocolException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+
+            ArrayList<FuelPriceModel> Models = new ArrayList();
         FuelPriceModel Model = new FuelPriceModel();
         JSONObject JSON;
         try{
-            JSON = (JSONObject) new JSONTokener(Input).nextValue();
+            JSON = (JSONObject) new JSONTokener(result).nextValue();
             JSONArray Array = JSON.getJSONArray("stations");
             for (int i =0 ; i < 8; i++)
             {
@@ -38,7 +103,7 @@ public class FuelSourceParser {
                     lat = Double.parseDouble(SLat);
                     lng = Double.parseDouble(SLng);
                     // LatLng Loc = new LatLng(lat,lng);
-                    Location location = null;
+                    Location location = new Location("StationLocation");
                     location.setLatitude(lat);
                     location.setLongitude(lng);
                     Model.setStationID(Name);
@@ -61,3 +126,5 @@ public class FuelSourceParser {
         return Models;
     }
 }
+// http://api.mygasfeed.com /stations/radius/(Latitude)/(Longitude)/(distance)/(fuel type)/(sort by)/z7lq1tt911.json
+//                                                                                                   api key        ?callback=?
